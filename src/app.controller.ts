bdce5e92a8b52f75ef1bff9d1bf9b5a0e08e2652
TuVoXuan <voxuantu1235@gmail.com>
2025-12-17 16:09:41 +0700

    feat: add app config and database connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectConnection() private connection: Connection,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health/db')
+  getDbHealth() {
+    return {
+      status: this.connection.readyState === 1 ? 'connected' : 'disconnected',
+      database: this.connection.name,
+      host: this.connection.host,
+      readyState: this.connection.readyState,
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './config/config.type';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import validationOptions from './utils/validation-options';
+import { ResolvePromisesInterceptor } from './utils/serializer.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService<AllConfigType>);
+
+  app.enableShutdownHooks();
+  app.setGlobalPrefix(configService.get('app.apiPrefix', { infer: true }), {
+    exclude: ['/'],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+  app.useGlobalInterceptors(
+    // ResolvePromisesInterceptor is used to resolve promises in responses because class-transformer can't do it
+    // https://github.com/typestack/class-transformer/issues/549
+    new ResolvePromisesInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
+
+  const options = new DocumentBuilder()
+    .setTitle('Askly API Gateway')
+    .setDescription('API docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
 bootstrap();
